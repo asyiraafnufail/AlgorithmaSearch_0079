725e8b135477ecb5cacb7d40945d14b901b371dd
asyiraafnufail <asyiraaf4@gmail.com>
2024-04-05 14:52:11 +0700

    menuliskan cout dan cin continue search

--- AlgorithmaSearch_0079.cpp
+++ AlgorithmaSearch_0079.cpp
@@ -58,4 +58,7 @@ void LinearSearch ()
             cout << "\n" << item << " not found in the array \n";
             cout << "\nNumber of comparisons : " << ctr << endl;
 
+            cout << "\nContinue search? (y/n) : ";
+            cin >> ch;
+
 }
